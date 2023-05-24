6d18331578a2ab7224953dde080069653afe8f31
Natalia Vorobjeva <vorobjeva.natalia76@yandex.ru>
2023-05-24 19:28:21 +0300

    Создать схему movie, установить зависимости

---
+++ models/movie.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
+require('mongoose-type-url');
+
+const { imgUrlRegExp } = require('../utils/regexp');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (image) => imgUrlRegExp.test(image),
+      message: 'Неверный URL-адрес',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (trailerLink) => imgUrlRegExp.test(trailerLink),
+      message: 'Неверный URL-адрес',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (thumbnail) => imgUrlRegExp.test(thumbnail),
+      message: 'Неверный URL-адрес',
+    },
+  },
+  owner: {
+    type: String,
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+
+}, { versionKey: false });
+
+module.exports = mongoose.model('movie', movieSchema);
