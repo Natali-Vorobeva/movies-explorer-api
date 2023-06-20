fb48b661c86eecd8d456df9215a96bca7829b999
Natalia Vorobjeva <vorobjeva.natalia76@yandex.ru>
2023-06-20 16:08:50 +0300

    Добавить allowedCors

--- utils/corsConfig.js
+++ utils/corsConfig.js
@@ -1,14 +1,6 @@
 const cors = require('cors');
 
 module.exports.corsConfig = cors({
-  option: [
-    'http://localhost:3000',
-    'https://diplom-portfolio-vorobeva.nomoredomains.rocks',
-    'http://diplom-portfolio-vorobeva.nomoredomains.rocks',
-    'https://api.portfolio-vorobeva.nomoredomains.rocks',
-    'http://api.portfolio-vorobeva.nomoredomains.rocks',
-
-  ],
   origin: [
     'http://localhost:3000',
     'https://diplom-portfolio-vorobeva.nomoredomains.rocks',
